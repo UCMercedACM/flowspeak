7cb0666487e42db5384ba1a0021c99b3b11ed1a9
Javier <javier.miranda4420@gmail.com>
2024-10-20 03:18:44 -0700

    began frontend

--- app/App.js
+++ app/App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Differentiate between setup and layout components
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import { Text, View, StatusBar } from "react-native";
+
+import Layout from "./layouts/layout";
+import Setup from "./layouts/setup";
+
+import * as Font from "expo-font";
+
+import API from "./api";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      setup: false,
+      font: false,
+    };
+  }
+
+  componentWillMount() {
+    API.getData("setup").then((setupStatus) => {
+      console.log(setupStatus);
+      if (setupStatus == "start") {
+        this.setState({ setup: "start" });
+      } else {
+        this.setState({ setup: "done" });
+      }
+    });
+  }
+
+  componentDidMount() {
+    Font.loadAsync({
+      rubik: require("./fonts/Rubik-Regular.ttf"),
+      "rubik-bold": require("./fonts/Rubik-Bold.ttf"),
+    }).then(() => {
+      this.setState({ font: true });
+    });
+    this.checkTimeout();
+  }
+
+  checkTimeout() {
+    setTimeout(() => {
+      if (!this.state.setup) {
+        API.getData("setup").then((setupStatus) => {
+          if (setupStatus == "start") {
+            this.setState({ setup: "start" });
+          } else {
+            this.setState({ setup: "done" });
+          }
+        });
+        this.checkTimeout();
+      }
+    }, 300);
+  }
+
+  setupFinished() {
+    this.setState({ setup: "done" });
+    API.setData("setup", "done");
+  }
+
+  renderMainComponent() {
+    if (this.state.setup == "start") {
+      return <Setup finished={this.setupFinished.bind(this)} />;
+    } else if (this.state.setup == "done") {
+      return <Layout language={this.state.currentLang} />;
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    if (this.state.font) {
+      return (
+        <View style={{ flex: 1, position: "relative" }}>
+          <StatusBar hidden={true} />
+          {this.renderMainComponent()}
+        </View>
+      );
+    } else {
+      return null;
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/README.md
@@ -0,0 +1,3 @@
+## Migrate app.json file
+# also App.test.js
+check what else needs to be migrated
